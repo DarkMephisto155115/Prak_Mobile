9f0d156f9158b4427b94167c70aa5e839feea8ae
Andika Salsabilah <andika.as995@gmail.com>
2024-12-26 03:43:56 +0700

    add icon and fix some merge shenanigans and name too. PlEASE KILL ME

--- lib/presentation/controllers/profile_controller.dart
+++ lib/presentation/controllers/profile_controller.dart
@@ -16,8 +16,7 @@ class ProfileController extends GetxController {
   var following = 0.obs;
   var stories = <Map<String, dynamic>>[].obs;
 
- var length = 0.obs;
-
+  var length = 0.obs;
 
 
   @override
@@ -25,75 +24,74 @@ class ProfileController extends GetxController {
     super.onInit();
     _getUserData();
     fetchStories();
-
-  Future<void> _getUserData() async {
-    String? localUserId = await getLocalData('userId');
-    print("user Id: $localUserId");
-    if (localUserId == null) {
-      print("Tidak ada user ID di SharedPreferences");
-      return;
-    }
-
-    userID.value = localUserId;
-
-    Map<String, dynamic>? userData = await getDataFirestore(localUserId);
-    print(userData);
-
-    if (userData != null) {
-      name.value = userData['name'] ?? '';
-      username.value = userData['username'] ?? '';
-      imagesURL.value =
-          userData['imagesURL'] ?? 'assets/images/default_profile.jpeg';
-      coins.value = userData['coins'] ?? 0;
-      followers.value = userData['followers'] ?? 0;
-      following.value = userData['following'] ?? 0;
-    } else {
-      print("Data user tidak ditemukan di Firestore");
-    }
   }
 
-  Future<String?> getLocalData(String key) async {
-    try {
-      SharedPreferences prefs = await SharedPreferences.getInstance();
-      return prefs.getString(key);
-    } catch (e) {
-      print("Error accessing SharedPreferences: $e");
-      return null;
-    }
-  }
+    Future<void> _getUserData() async {
+      String? localUserId = await getLocalData('userId');
+      print("user Id: $localUserId");
+      if (localUserId == null) {
+        print("Tidak ada user ID di SharedPreferences");
+        return;
+      }
 
-  Future<Map<String, dynamic>?> getDataFirestore(String userId) async {
-    try {
-      DocumentSnapshot<Map<String, dynamic>> document =
-          await _firestore.collection('users').doc(userId).get();
-      if (document.exists) {
-        print(document.data());
-        return document.data()!;
+      userID.value = localUserId;
+
+      Map<String, dynamic>? userData = await getDataFirestore(localUserId);
+      print(userData);
+
+      if (userData != null) {
+        name.value = userData['name'] ?? '';
+        username.value = userData['username'] ?? '';
+        imagesURL.value =
+            userData['imagesURL'] ?? 'assets/images/default_profile.jpeg';
+        coins.value = userData['coins'] ?? 0;
+        followers.value = userData['followers'] ?? 0;
+        following.value = userData['following'] ?? 0;
       } else {
-        print("Data tidak ditemukan dari id: $userId");
+        print("Data user tidak ditemukan di Firestore");
+      }
+    }
+
+    Future<String?> getLocalData(String key) async {
+      try {
+        SharedPreferences prefs = await SharedPreferences.getInstance();
+        return prefs.getString(key);
+      } catch (e) {
+        print("Error accessing SharedPreferences: $e");
         return null;
       }
-    } catch (e) {
-      print("Error fetching data: $e");
-      return null;
+    }
+
+    Future<Map<String, dynamic>?> getDataFirestore(String userId) async {
+      try {
+        DocumentSnapshot<Map<String, dynamic>> document =
+        await _firestore.collection('users').doc(userId).get();
+        if (document.exists) {
+          print(document.data());
+          return document.data()!;
+        } else {
+          print("Data tidak ditemukan dari id: $userId");
+          return null;
+        }
+      } catch (e) {
+        print("Error fetching data: $e");
+        return null;
+      }
+    }
+
+    Future<void> fetchStories() async {
+      try {
+        final querySnapshot = await FirebaseFirestore.instance
+            .collection('stories')
+            .where('writerId', isEqualTo: userID.value)
+            .get();
+
+        // Update the reactive list with fetched stories
+        stories.value = querySnapshot.docs
+            .map((doc) => {'id': doc.id, ...doc.data() as Map<String, dynamic>})
+            .toList();
+      } catch (e) {
+        print('Error fetching stories: $e');
+      }
     }
   }
-
-  Future<void> fetchStories() async {
-    try {
-      final querySnapshot = await FirebaseFirestore.instance
-          .collection('stories')
-          .where('writerId', isEqualTo: userID.value)
-          .get();
-
-      // Update the reactive list with fetched stories
-      stories.value = querySnapshot.docs
-          .map((doc) => {'id': doc.id, ...doc.data() as Map<String, dynamic>})
-          .toList();
-
-
-    } catch (e) {
-      print('Error fetching stories: $e');
-    }
-  }
-}
